96a271b7aa5ba5e3985ba0230ec39d38241f1aae
Efireon <daner12258@gmail.com>
2026-06-22 23:55:35 +0300

    hello: добавлена на обработка inline_keyboard на стартовое сообщение

--- src/bot/hello.py
+++ src/bot/hello.py
@@ -2,12 +2,17 @@ from aiogram import Router, html
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from bot.buttons import keyboard
+
 router = Router()
 
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}!")
+    await message.answer(
+        f"Привет, {html.bold(message.from_user.full_name)}!",
+        reply_markup=keyboard,
+    )
 
 
 @router.message()
