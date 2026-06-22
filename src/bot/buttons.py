9a0c8bd4135cc7ad4022b0db3b09d71a67d2c16d
Efireon <daner12258@gmail.com>
2026-06-22 23:51:21 +0300

    buttons: - переписана часть кода на используемую библиотеку(aiogram) - убраны whitespace-ы

--- src/bot/buttons.py
+++ src/bot/buttons.py
@@ -1,35 +1,17 @@
-import asyncio
-import logging
-import sys
-import telegram
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-from os import getenv
-from dotenv import load_dotenv
+from aiogram import Router
+from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
+router = Router()
 
-
-
-
-
-
-
-
-
-
-
-
-def start(update, context):
-    keyboard = [
-        [InlineKeyboardButton("Кнопка 1", callback_data= '1'),
-        InlineKeyboardButton("Кнопка 2", callback_data='2')]
+keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text="Кнопка 1", callback_data="1"),
+        InlineKeyboardButton(text="Кнопка 2", callback_data="2"),
     ]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Выберите кнопку:', reply_markup=reply_markup)
+])
 
 
-def button(update, context):
-    query = update.callback_query
-    query.answer()
-    query.edit_message_text(text=f"Вы нажали кнопку: {query.data}")
+@router.callback_query()
+async def button_handler(query: CallbackQuery) -> None:
+    await query.answer()
+    await query.edit_message_text(text=f"Вы нажали кнопку: {query.data}")
