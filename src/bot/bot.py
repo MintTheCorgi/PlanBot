fa99404fbfdef0262a37ac3d0014693af69dc5f5
MintSpaceCorgi <denuvofirewall@gmail.com>
2026-06-22 18:08:32 +0300

    Добавлены тестовые кнопки

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,6 +1,9 @@
 import asyncio
 import logging
 import sys
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from os import getenv
 from dotenv import load_dotenv
 
@@ -23,8 +26,33 @@ dp.include_router(hello_router)
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot)
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
 
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CallbackQueryHandler(button))
+
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
+
+
+
+
+def start(update, context):
+    keyboard = [
+        [InlineKeyboardButton("Кнопка 1", callback_data= '1'),
+        InlineKeyboardButton("Кнопка 2", callback_data='2')]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Выберите кнопку:', reply_markup=reply_markup)
+
+
+def button(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text=f"Вы нажали кнопку: {query.data}")
