c6bbd2238c52d1beea75c16e2cc93a7f2dca7dba
Efireon <daner12258@gmail.com>
2026-06-22 23:49:53 +0300

    bot: - убран шаблонный PTB код( не та библиотека) - добавлено включение buttons

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,9 +1,6 @@
 import asyncio
 import logging
 import sys
-import telegram
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from os import getenv
 from dotenv import load_dotenv
 
@@ -12,6 +9,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from bot.hello import router as hello_router
+from bot.buttons import router as buttons_router
 
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
@@ -21,38 +19,14 @@ if not TOKEN:
 
 dp = Dispatcher()
 dp.include_router(hello_router)
+dp.include_router(buttons_router)
 
 
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot)
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CallbackQueryHandler(button))
-
-    updater.start_polling()
-    updater.idle()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-
-
-
-
-def start(update, context):
-    keyboard = [
-        [InlineKeyboardButton("Кнопка 1", callback_data= '1'),
-        InlineKeyboardButton("Кнопка 2", callback_data='2')]
-    ]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Выберите кнопку:', reply_markup=reply_markup)
-
-
-def button(update, context):
-    query = update.callback_query
-    query.answer()
-    query.edit_message_text(text=f"Вы нажали кнопку: {query.data}")
